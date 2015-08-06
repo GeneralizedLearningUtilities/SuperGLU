a6e1ad72c75f1fd2182cbb07c7c40507049bd8fb
benjamid <bdnye@memphis.edu>
2015-08-05 19:47:28 -0700

    Added (somewhat) better documentation to the classifier model builders. These are basically wrappers around real classifier that are intended to handle creating feature sets based on instances, so that a matrix suitable for machine learning algorithms is created.  There are also some specific model wrappers that show a linear classifier and SVM, from SciKit Learn.

--- Services/Classifiers/ClassifierModels.py
+++ Services/Classifiers/ClassifierModels.py
@@ -7,7 +7,20 @@ from SuperGLU.Classifiers.ModelFeature import FeatureSelector, ModelFeature
 # Builder for Classifires
 #------------------------------
 class ClassifierBuilder(object):
+    """
+    Class for building a classifier from data, which creates both the feature set and
+    the trained classifier from the available data.
+    """
     def __init__(self, modelMaker, features=None, params=None, modelParams=None):
+        """
+        Initialize the Classifier Builder
+        @param modelMaker: A callable class that creates a trainable model with
+                           certain features and expected model params
+        @param featureTypes: A list of categories for of feature that could be created, where one
+                             feature type might make many features (e.g., "every n-tuple of words that occurs")
+        @param params: Parameters for evaluating feature types
+        @param modelParams: Parameters to pass in to the model maker (e.g., update rates, convergence criteria)
+        """
         if features is None: features = []
         if params is None: params = {}
         if modelParams is None: modelParams = {}  
@@ -16,10 +29,15 @@ class ClassifierBuilder(object):
         self._params = params
         self._modelParams = modelParams
 
+    # ------------------------------------------------
+    # Functions to Generate a Set of Possible Features
+    # ------------------------------------------------
     def getFeatureTypes(self):
+        """ The names of the types of features to create """
         return [x.getName() for x in self._featureTypes]
 
     def selectAllFeatures(self, inputs, outputs=None):
+        """ Identify the set of possible features across all feature types """
         allFeatures = []
         for featureType in self._featureTypes:
             features = self.selectFeatures(featureType, inputs, outputs)
@@ -28,6 +46,7 @@ class ClassifierBuilder(object):
         return allFeatures
 
     def selectFeatures(self, featureType, inputs, outputs=None):
+        """ Identify the set of possible features for a given feature type """
         if isinstance(featureType, FeatureSelector):
             features = featureType(inputs, outputs, self._params)
         elif isinstance(featureType, ModelFeature):
@@ -36,14 +55,11 @@ class ClassifierBuilder(object):
             raise TypeError('Invalid feature selector or feature, got: %s'%(featureType,))
         return features
 
-    def makeModel(self, features, inputs, outputs):
-        featureVals = sorted([(f.getName(), f) for f in features])
-        featureVals = [f for name, f in featureVals]
-        model = self._modelMaker(featureVals, dict(self._modelParams))
-        model.train(inputs, outputs)
-        return model
-
+    # -------------------------------------------------
+    # Functions to Calculate a Matrix of Feature Values
+    # -------------------------------------------------
     def calculateFeatureValues(self, inputs, outputs=None):
+        """ Calculate the matrix of feature values to use for training """
         features = self.selectAllFeatures(inputs, outputs)
         inputFeatureSets = []
         for inp in inputs:
@@ -52,6 +68,7 @@ class ClassifierBuilder(object):
         return inputFeatureSets
 
     def makeFeatureTable(self, inputs, outputs):
+        """ Create a table of features, where the outputs are added as a final column """
         features = self.selectAllFeatures(inputs, outputs)
         inputs = self.calculateFeatureValues(inputs, outputs)
         header = [f.getName() for f in features] + ['Output']
@@ -61,7 +78,19 @@ class ClassifierBuilder(object):
             outRecords.append(outRecord)
         return outRecords
 
+    # -------------------------------------------------
+    # Functions to Train Models based on Data
+    # -------------------------------------------------
+    def makeModel(self, features, inputs, outputs):
+        """ Create a trained model from the given features, inputs, and outputs """
+        featureVals = sorted([(f.getName(), f) for f in features])
+        featureVals = [f for name, f in featureVals]
+        model = self._modelMaker(featureVals, dict(self._modelParams))
+        model.train(inputs, outputs)
+        return model
+
     def __call__(self, inputs, outputs):
+        """ Run the Classifier Builder on data, selecting features and training a model with them """
         allFeatures = self.selectAllFeatures(inputs, outputs)
         model = self.makeModel(allFeatures, inputs, outputs)
         return model
@@ -69,43 +98,75 @@ class ClassifierBuilder(object):
 # Stored Classifiers
 #------------------------------
 class ClassifierModel(object):
+    """ Wrapper/base class for models that are trained based on some features """
+    
     def __init__(self, features, params=None):
+        """
+        Initialize the model
+        @param features: The set of features to calculate, based on some input data
+        @param params: Parameters needed to train the model
+        """
         if params is None: params = {}
         self._params = dict(params)
         self._features = features
 
     def __call__(self, value):
+        """
+        Turn an instance into features, then predict the output
+        @param value: Any valid object that the features can process
+        @return: Predicted output label
+        """
         features = self.makeFeatures(value)
         return self._predict(features)
 
     def train(self, inputs, outputs):
+        """
+        Train a classifier model
+        @param inputs: Input instances
+        @param outputs: Output labels
+        """
         features = [self.makeFeatures(anInput) for anInput in inputs]
         self._train(features, outputs)
 
     def getFeatureNames(self):
+        """ Return the names of all features """
         return [x.getName() for x in self._features]
 
     def getFeatureImportances(self):
+        """ Return the relative importance weights of all features, if available """
         return [(name, '?') for name in self.getFeatureNames()]
 
     def makeFeatures(self, anInput):
+        """ Turn an instance input into a vector of features """
         return [f(anInput) for f in self._features]
 
     def _predict(self, features):
+        """ Predict an output label based on feature values """
         raise NotImplementedError
 
     def _train(self, features, outputs):
+        """ Train some classifier model """
         pass
 
     def calcR2(self, inputs, outputs):
+        """ Calculate a basic R2 value for the given input-output set """
         vals = [self(anInput) for anInput in inputs]
         return calcR2(vals, outputs)
 
     def score(self, inputs, outputs):
+        """ Calculate a score for this model's predictions based on the input-output set """
         return self.calcR2(inputs, outputs)
 
+
 class LinearClassModel(ClassifierModel):
+    """ Wrapper for a linear classifier model """
+    
     def __init__(self, features, params=None):
+        """
+        Initialize the classifier
+        @param valMin: A minimum value to apply to the output (floor)
+        @param valMax: A maximum value to apply to the output (ceiling)
+        """
         if params is None: params = {}
         params = dict(params)
         self._valMin = params.pop('ValMin', None)
@@ -114,15 +175,18 @@ class LinearClassModel(ClassifierModel):
         self._model = None
 
     def getFeatureImportances(self):
+        """ Return feature importances, which are the coefficients """
         return [(name, self._model.coef_[i]) for i, name in enumerate(self.getFeatureNames())]
     
     def _train(self, features, outputs):
+        """ Fit a linear regression model """
         #est = LinearRegression(**self._params)
         est = Ridge(**self._params)
         est.fit(features, outputs)
         self._model = est
 
     def _predict(self, features):
+        """ Predict labels for the feature vector, applying the min-max as bounds """
         val = float(self._model.predict([features]))
         if self._valMin  is not None:
             val = max(self._valMin, val)
@@ -130,18 +194,39 @@ class LinearClassModel(ClassifierModel):
             val = min(self._valMax, val)
         return val
 
+
 class SVMModel(ClassifierModel):
+    """ Wrapper for an SVM model """
+    DEFAULT_KERNEL = 'linear'
+    DEFAULT_C = 1.0
+    DEFAULT_WEIGHT = 'auto'
+    
     def __init__(self, features, params=None):
+        """
+        Initialize the SVM Model wrapper
+        @param OutputTransform: A transform function to the apply to the SVM model output (e.g., binning)
+        """
+        self._outputTransform = params.pop('OutputTransform', lambda x: x)
         super(SVMModel, self).__init__(features, params)
         self._model = None
-        self._outputTransform = params.get('OutputTransform', lambda x: x)
+        self._loadDefaultParams(False)
+
+    def _loadDefaultParams(self, overwrite=False):
+        params = {'kernel' : self.DEFAULT_KERNEL,
+                  'C' : self.DEFAULT_C,
+                  'class_weight' : 'auto'}
+        for k, v in params.items():
+            if overwrite or k not in self._params:
+                self._params[k] = v
     
     def _train(self, features, outputs):
+        """ Train an SVM model, transforming the features first """
         outputs = [self._outputTransform(o) for o in outputs]
         est = svm.SVC(kernel='linear', C=1.0, class_weight='auto')
         est.fit(features, outputs)
         self._model = est
 
     def _predict(self, features):
+        """ Predict a value using the SVM model """
         return float(self._model.predict([features]))
 
