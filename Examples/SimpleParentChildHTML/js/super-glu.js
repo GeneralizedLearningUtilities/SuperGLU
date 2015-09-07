693ba28af7bc3e0ac19939cc0f3f2ebb60643aae
benjamid <bdnye@memphis.edu>
2015-09-07 15:06:31 -0700

    Corrected version number.  Also, created a second, more advanced implementation with three frames.  Finally, added functionality to pass the name of the child frame gateway ID as a URL parameter.  Still looking for the most convenient way to ensure that all postMessage frame gateways have unique IDs that are all easily known by their adjacent frames (but that are still unique across any arbitrary level of frames or servers, e.g., UUID's).

--- Examples/SimpleParentChildHTML/js/super-glu.js
+++ Examples/SimpleParentChildHTML/js/super-glu.js
@@ -18,7 +18,7 @@ if (typeof window === "undefined") {
 }
 
 (function(namespace, undefined) {
-namespace.version = "0.6.0";
+namespace.version = "0.1.0";
 
 // Core API Modules
 if ((namespace.Zet == null) && typeof Zet !== 'undefined'){ 
