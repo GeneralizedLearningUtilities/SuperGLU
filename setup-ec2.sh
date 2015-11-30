b4e9b89d1680d6a457c28e6ad5467c6bf4a8e211
dan100110 <daniel.auerbach@gmail.com>
2015-11-30 14:58:43 -0800

    minor modification.

--- setup-ec2.sh
+++ setup-ec2.sh
@@ -1,9 +1,7 @@
 sudo apt-get -y install python3
 sudo apt-get -y install gcc
 sudo apt-get -y install python-dev
-sudo apt-get -y install git
 sudo apt-get install -qy python-setuptools
 sudo easy_install pip
 pip install virtualenv
 
-git clone https://github.com/GeneralizedLearningUtilities/SuperGLU superglu
