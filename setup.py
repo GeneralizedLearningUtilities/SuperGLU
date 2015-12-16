48a8c67e6ff35b8604e9444a5b65b2d92213783e
Ben Nye <benjamin.nye@gmail.com>
2015-12-15 16:54:58 -0800

    Adding files to add to PyPy.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'SuperGLU',
+  packages = ['SuperGLU'],
+  version = '0.1.0',
+  description = 'Base Generalized Learning Utilities (GLU) library for communicating data between different learning technologies and intelligent systems.',
+  author = 'Benjamin D. Nye',
+  author_email = 'benjamin.nye@gmail.com',
+  url = 'https://github.com/GeneralizedLearningUtilities/SuperGLU',
+  download_url = 'https://github.com/GeneralizedLearningUtilities/SuperGLU/tarball/0.1',
+  keywords = ['ITS', "Adaptive Learning", 'Messaging', 'HTML5', "Websockets", "Service"],
+  classifiers = [          
+		  "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python",
+		  "Programming Language :: JavaScript",
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Science/Research"],
+)
