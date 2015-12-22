268b508fda68439a468b70a4b768d2a70c699185
benjamid <bdnye@memphis.edu>
2015-12-21 22:04:45 -0800

    Switching to main PyPI index.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'SuperGLU',
   packages = ['SuperGLU'],
-  version = '0.1.0',
+  version = '0.1.1',
   description = 'Base Generalized Learning Utilities (GLU) library for communicating data between different learning technologies and intelligent systems.',
   author = 'Benjamin D. Nye',
   author_email = 'benjamin.nye@gmail.com',
