f7e25336e6e24c453578e177f88095aa324b651e
dan100110 <daniel.auerbach@gmail.com>
2016-01-27 17:22:13 -0800

    added a few comments for clarity

--- SuperGLU/Services/QueryService/Queries.py
+++ SuperGLU/Services/QueryService/Queries.py
@@ -3,6 +3,8 @@ from SuperGLU.Util.Serialization import Serializable
 from SuperGLU.Core.MessagingGateway import BaseService
 from SuperGLU.Core.Messaging import Message, MessageLite
 
+#These classes are intended to allow the user to access data stored from the messages.
+
 class LearnerDataQueryBase (BaseService) :
     
     def receiveMessage(self, msg):
@@ -10,13 +12,18 @@ class LearnerDataQueryBase (BaseService) :
         
     def runQuery(self, value):
         raise NotImplementedError("this class is abstract, use the subclasses")
-        
+    
+    #return results of query as a list of Message objects, not MessageLite objects.
     def runQueryInternal(self, indexName, value):
         messageLiteList = MessageLite.find_by_index(indexName, value)
         result = [];
+        #There should be a better way to do this using some of python's functional programming features, but I couldn't find it.
         for messageLite in messageLiteList:
             result.append(messageLite.toMessage())
         return result;
+        
+        
+#Basic Queries
         
 class LearnerDataQueryByActor(LearnerDataQueryBase):  
     def runQuery(self, value):
@@ -31,3 +38,6 @@ class LearnerDataQueryByVerb(LearnerDataQueryBase):
 class LearnerDataQueryByObject(LearnerDataQueryBase):
     def runQuery(self, value):
         return super(LearnerDataQueryByObject, self).runQueryInternal('objectIndex', value)
+        
+        
+#More advanced Queries
