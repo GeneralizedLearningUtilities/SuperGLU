4cdcdb414faec9a6ef514973feeaaf15f43fa776
dan100110 <daniel.auerbach@gmail.com>
2016-01-29 17:26:27 -0800

    wrote a function to filter query results based on a dummy message

--- SuperGLU/Core/MessagingDB.py
+++ SuperGLU/Core/MessagingDB.py
@@ -1,5 +1,5 @@
 from gludb.simple import DBObject, Field, Index
-
+from SuperGLU.Core.Messaging import Message
 """
 Module for storing the class that persists the messaging objects into the database.
 """
@@ -41,6 +41,37 @@ class DBLoggedMessage(object):
         
     def toMessage(self):
         return Message(self.actor, self.verb, self.object, self.result, self.speechAct, self.context, self.timestamp)
+    
+    def matchOnPartial(self, current):
+        if self.actor is not None and current.actor != self.actor:
+            return False
+        
+        if self.verb is not None and current.verb != self.verb:
+            return False
+            
+        if self.object is not None and current.object != self.object:
+            return False
+            
+        if self.speechAct is not None and current.speechAct != current.speechAct:
+            return False
+        
+        if self.result is not None and current.result != self.result:
+            return False
+        
+        if self.context is not None:
+            if current.context is None:
+                return False
+            
+            #Note: I am assuming that the context is a dictionary if that isn't true then I'll need to add a type check and handle all possible types 
+            for otherContextKey in self.context.keys:
+                if otherContextKey not in current.context and self.context[otherContextKey] != current.context[otherContextKey] :
+                    return False
+        
+        if self.timestamp is not None and current.timestamp != self.timestamp:
+            return False;
+        
+        return True
+        
         
     def __repr__(self):
         return self.actor + "|" + self.verb + "|" + self.object + "|" + self.result.__repr__() + "|" + self.speechAct + "|" + self.context.__repr__() + "|" +self.timestamp + "\n"
--- SuperGLU/Services/QueryService/Queries.py
+++ SuperGLU/Services/QueryService/Queries.py
@@ -3,6 +3,7 @@ from SuperGLU.Util.Serialization import Serializable
 from SuperGLU.Core.MessagingGateway import BaseService
 from SuperGLU.Core.Messaging import Message
 from SuperGLU.Core.MessagingDB import DBLoggedMessage
+from itertools import filterfalse
 
 #These classes are intended to allow the user to access data stored from the messages.
 
@@ -10,33 +11,45 @@ class LearnerDataQueryBase (BaseService) :
         
     def runQuery(self, value):
         raise NotImplementedError("this class is abstract, use the subclasses")
-    
-    #return results of query as a list of Message objects, not DBLoggedMessage objects.
-    def runQueryInternal(self, indexName, value):
-        dbLoggedMessageList = DBLoggedMessage.find_by_index(indexName, value)
-        result = [dbLoggedMessage.toMessage() for dbLoggedMessage in dbLoggedMessageList
-                  if dbLoggedMessage is not None]
-        return result
+
+    #filter the results of a query to match a partially filled out message
+    def filterQueryResults(self, queryResults, filter):
+        filterfalse(lambda x : filter.matchOnPartial(x), queryResults)
+        return queryResults
         
+    #return results of query as a list of DBLoggedMessage objects.
+    def runQueryInternal(self, indexName, value):
+        return DBLoggedMessage.find_by_index(indexName, value)
+        
+        
+    #convert results from DBLoggedMessage to Message
+    def convertResultsToMessageList(self, dbLoggedMessageList):
+        result = [dbLoggedMessage.toMessage() for dbLoggedMessage in dbLoggedMessageList
+                    if dbLoggedMessage is not None]
+        return result
         
 #Basic Queries
         
 class LearnerDataQueryByActor(LearnerDataQueryBase):  
     def runQuery(self, value):
-        return super(LearnerDataQueryByActor, self).runQueryInternal('actorIndex', value);
+        dbLoggedMessageList = super(LearnerDataQueryByActor, self).runQueryInternal('actorIndex', value);
+        print(dbLoggedMessageList)
+        filter = DBLoggedMessage(actor=None, verb="Completed", object=None, result=None, speechAct=None, context=None)
+        print (super(LearnerDataQueryByActor, self).filterQueryResults(dbLoggedMessageList, filter))
         
+        return super(LearnerDataQueryByActor, self).convertResultsToMessageList(dbLoggedMessageList)
         
 class LearnerDataQueryByVerb(LearnerDataQueryBase):
     def runQuery(self, value):
-        return super(LearnerDataQueryByVerb, self).runQueryInternal('verbIndex', value)
+        dbLoggedMessageList = super(LearnerDataQueryByVerb, self).runQueryInternal('verbIndex', value)
+        return super(LearnerDataQueryByVerb, self).convertResultsToMessageList(dbLoggedMessageList)
         
         
 class LearnerDataQueryByObject(LearnerDataQueryBase):
     def runQuery(self, value):
-        return super(LearnerDataQueryByObject, self).runQueryInternal('objectIndex', value)
+        dbLoggedMessageList = super(LearnerDataQueryByObject, self).runQueryInternal('objectIndex', value)
+        return super(LearnerDataQueryByObject, self).convertResultsToMessageList(dbLoggedMessageList)
         
         
 #More advanced Queries
 
-#class SingleStudentQueryBy(LearnerDataQueryBase):
- #   def runQuery(self, value):
