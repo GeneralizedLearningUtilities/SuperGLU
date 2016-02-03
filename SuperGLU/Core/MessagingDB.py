2f497732c56da344585f68909904831502828088
dan100110 <daniel.auerbach@gmail.com>
2016-02-03 14:52:33 -0800

    copied messaging constants from standard-its-logging.js to the python code.  We will need them eventually and they can be more easily moved to a more appropriate location if necessary.

--- SuperGLU/Core/MessagingDB.py
+++ SuperGLU/Core/MessagingDB.py
@@ -5,10 +5,55 @@ from datetime import datetime
 Module for storing the class that persists the messaging objects into the database.
 """
 
-USER_ID_CONTEXT_KEY = 'userId'
-TASK_ID_CONTEXT_KEY = 'taskId'
-STEP_ID_CONTEXT_KEY = 'stepId'
-# User + Task as an index also
+LOADED_VERB = "Loaded";
+    
+COMPLETED_VERB = 'Completed'                       # Finished task, return result (e.g., score)
+COMPLETED_ALL_STEPS_VERB = 'CompletedAllSteps'     # Completed all steps (true/false/% steps completed)
+COMPLETED_STEP_VERB = 'CompletedStep'              # Completed a given task step
+KC_SCORE_VERB = 'KnowledgeComponentScore'          # A score for a KC (e.g., performance on a given task)
+
+# Task Adaptive Support Verbs
+TASK_HELP_VERB = 'TaskHelp'                        # User received some other type of help on a task
+TASK_HINT_VERB = 'TaskHint'                        # User received a hint (e.g., next step) on a task
+TASK_FEEDBACK_VERB = 'TaskFeedback'                # User received reactive feedback (e.g., error correction, approval)
+TASK_DECOMPOSITION_VERB = 'TaskDecomposition'      # User received a decomposed task (i.e., broken into subtasks)
+    
+# Task User Input Verbs
+PRESENTED_VERB = 'Presented'                       # User was presented with some element
+SELECTED_OPTION_VERB = 'SelectedOption'            # User selected some option or element
+SUBMITTED_ANSWER_VERB = 'SubmittedAnswer'          # An answer submitted by a user
+MISCONCEPTION_VERB = 'Misconception'               # User demonstrated a specific bug or misconception
+TASK_SUPPORT_VERB = 'TaskSupport'                  # Overall level of support given to the user during task
+TASK_HELP_COUNT_VERB = 'TaskHelpCount'             # Overall number of hints user received during the task.
+ 
+#Peripheral Metrics Verbs - Calculated by Task
+WORDS_PER_SECOND_VERB = 'WordsPerSecond'           # # Words per second, for text-input interactions (e.g., natural language ITS)
+ACTIONS_PER_SECOND_VERB = 'ActionsPerSecond'       # # Actions per second (e.g., selecting choices, attempting to answer) 
+ANSWER_SEMANTIC_MATCH_VERB = 'AnswerSemanticMatch' # Match of an answer for the user to some ideal(s)
+PERSISTENCE_VERB = 'Persistence'                   # Metric for persistence (e.g., continuing despite failure)
+IMPETUOUSNESS_VERB = 'Impetuousness'               # Metric for impetuousness (e.g., answering overly quickly, carelessness)
+GAMING_SYSTEM_VERB = 'GamingTheSystem'             # Metric for gaming the system (e.g., hint abuse)
+WHEELSPINNING_VERB = 'WheelSpinning'               # Metric for wheel spinning (e.g., continued failure on similar skills w/o improvement)
+CONFUSION_VERB = 'Confusion'                       # Metric for confusion (e.g., moderate delays, poor answer cohesion, video metrics)
+DISENGAGEMENT_VERB = 'Disengagement'               # Metric for disengagement (e.g., long delays, inattentive gaze, leaning back in chair)
+MASTERY_VERB = 'Mastery'
+
+#Context Keys
+USER_ID_CONTEXT_KEY = 'userId'                             # Unique identifier for the user
+DURATION_CONTEXT_KEY = 'duration'                          # Duration spent on the task, 
+TASK_ID_CONTEXT_KEY = 'taskId'                             # ID for the task being performed
+STEP_ID_CONTEXT_KEY = 'stepId'                             # Unique ID for the current step or state.
+ACTIVITY_TYPE_CONTEXT_KEY = 'activityType'                 # Type of activity being performed
+KC_RELEVANCE_CONTEXT_KEY = 'KCRelevance'                   # Knowledge component relevance
+HELP_TYPE_CONTEXT_KEY = 'helpType'                         # Type of the help provided (e.g., Positive, Negative, neutral)
+RESULT_CONTENT_TYPE_CONTEXT_KEY = 'resultContentType'      # Type for the content of the help or other media (e.g., 'text', 'html', 'jpg')     
+    
+#Other Constants
+UNKNOWN_PREFIX = 'Unknown'
+POSITIVE_HELP_TYPE = 'Positive'
+NEUTRAL_HELP_TYPE = 'Neutral'
+NEGATIVE_HELP_TYPE = 'Negative'
+ 
 
 @DBObject(table_name="DBLoggedMessage")
 class DBLoggedMessage(object):
