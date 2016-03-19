c5ac8d5cf450b0e09e047cd8748b5a8ac30ae080
dan100110 <daniel.auerbach@gmail.com>
2016-03-18 18:20:58 -0700

    started work on the messaging portion of the CSVReader

--- SuperGLU/Services/CSVReader/CSVReader.py
+++ SuperGLU/Services/CSVReader/CSVReader.py
@@ -4,8 +4,15 @@ This module is responsible for converting the tasks csv file to tasks objects an
 @author: auerbach
 '''
 from SuperGLU.Core.MessagingGateway import BaseService
-from SuperGLU.Services.StudentModel.PersistentData import SerializableTask,\
-    SerializableAssistmentsItem
+from SuperGLU.Services.StudentModel.PersistentData import SerializableTask, SerializableAssistmentsItem
+from SuperGLU.Core.FIPA.SpeechActs import INFORM_ACT
+from SuperGLU.Util.ErrorHandling import logInfo
+from SuperGLU.Core.Messaging import Message
+from SuperGLU.Services.Authentication.UserDataService import VALUE_VERB
+from SuperGLU.Util.Serialization import serializeObject
+
+
+CSV_READER_SERVICE_NAME = "CSV Reader Service"
 
 class CSVReader (BaseService):
     
@@ -14,13 +21,34 @@ class CSVReader (BaseService):
     PIPE_DELIMITER = '|||'
     
     
-    def receiveMessage(self, msg):
-        #TODO: implementation
-        raise NotImplementedError
     
+    def receiveMessage(self, msg):
+        #depending on the content of the message react differently
+        logInfo('Entering CSVReader.receiveMessage', 5)
+        
+        reply = None
+        #Only considering 
+        if msg.getSpeechAct() == INFORM_ACT:
+        
+            if True:
+                logInfo('{0} is processing a {1},{2} message'.format(CSV_READER_SERVICE_NAME, CSV_READER_SERVICE_NAME, INFORM_ACT), 4)
+                
+                csvString = msg.getResult()
+                taskList = self.processCSVFile(csvString)
+                
+                reply = Message()
+                reply.setSpeechAct(INFORM_ACT)
+                reply.setVerb(VALUE_VERB)
+                reply.setObject(taskList)    
+        
+        
+        if reply is not None:
+            logInfo('{0} is broadcasting a {1}, {2} message'.format(CSV_READER_SERVICE_NAME, INFORM_ACT, VALUE_VERB), 4)
+            self.sendMessage(reply)
     
     def processCSVFile(self, csvString):
         #List of strings
+        logInfo('Entering CSVReader.processCSVFile', 4)
         lines = csvString.split(sep=self.LINE_DELIMITER)
         
         #list of tasks
@@ -30,13 +58,18 @@ class CSVReader (BaseService):
         lines.pop(0)
         
         for line in lines:
+            
+            logInfo('{0} is splitting the line into cells'.format(CSV_READER_SERVICE_NAME), 5)
             cells = line.split(sep=self.CELL_DELIMITER)
             
             task = SerializableTask()
             
             
+            logInfo('{0} is extracting the knowledge components'.format(CSV_READER_SERVICE_NAME), 5)
             kcCell = cells[10]
             kcs = kcCell.split(self.PIPE_DELIMITER)
+            
+            logInfo('{0} is constructing the next serializable task object'.format(CSV_READER_SERVICE_NAME), 5)
             task._kcs = kcs
             
             task._name = cells[2]
@@ -51,6 +84,7 @@ class CSVReader (BaseService):
                 assistmentsItem._problemSetName = cells[13]
                 assistmentsItem._assignments = []
                 
+                logInfo('{0} is checking for assistments data'.format(CSV_READER_SERVICE_NAME), 5)
                 if cells[14] is not None and cells[14] is not '':
                     assistmentsItem._assignments.append((cells[14], cells[15], cells[16]))
                 if cells[17] is not None and cells[17] is not '':
@@ -62,8 +96,11 @@ class CSVReader (BaseService):
             else:
                 task._assistmentsItem = None 
             
-            
+            #this could cause performance trouble so only use this log if you need to.
+            #logInfo('{0} constructed task: {1}'.format(CSV_READER_SERVICE_NAME, serializeObject(task)), 5)
+            logInfo('{0} constructed task with name: {1}'.format(CSV_READER_SERVICE_NAME, task._name), 5)
             result.append(task)
-            
+        
+        logInfo('Exiting CSVReader.processCSVFile', 4)    
         return result
             
