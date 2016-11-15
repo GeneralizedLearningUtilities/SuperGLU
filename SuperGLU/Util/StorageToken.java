d1e835fe8702393486d2e048e2b2eaf571a4717c
dan100110 <daniel.auerbach@gmail.com>
2016-11-15 14:15:56 -0800

    Beginning the Java Port of SuperGLU.

---
+++ SuperGLU/Util/StorageToken.java
@@ -0,0 +1,185 @@
+/**
+ * This is the intermediate form of data that allows it to be serialized to multiple formats.
+ */
+package Util;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import Util.tokenformat.TokenRWFormat;
+
+public class StorageToken implements Iterable<String> {
+
+	
+	public static String ID_KEY = "id";
+	public static String CLASS_ID_KEY = "classId";
+	
+	public static Set<String> reservedKeys = new HashSet<>();
+	
+	private Map<String, Object> data;
+	
+	static
+	{
+		reservedKeys.add(CLASS_ID_KEY);
+		reservedKeys.add(ID_KEY);
+	}
+	
+	
+	public StorageToken(Map<String, Object> data, String id, String classId)
+	{
+		this.data = data;
+		
+		if(data == null)
+			this.data = new HashMap<>();
+		
+		if(id != null)
+		{
+			this.setId(id);
+		}
+		
+		else if(this.data.get(ID_KEY) == null)
+		{
+			this.data.put(ID_KEY, UUID.randomUUID().toString());
+		}
+		
+		if(classId != null)
+		{
+			this.setClassId(classId);
+		}
+		
+		
+		
+	}
+	
+	//Accessors
+	public String getId()
+	{
+		return (String) this.data.get(ID_KEY);
+	}
+	
+	
+	public void setId(String id)
+	{
+		this.data.put(ID_KEY, id);
+	}
+	
+	public String getClassId()
+	{
+		return (String) this.data.get(CLASS_ID_KEY);
+	}
+	
+	
+	public void setClassId(String classId)
+	{
+		this.data.put(CLASS_ID_KEY, classId);
+	}
+	
+	
+	public int getSize()
+	{
+		return data.size();
+	}
+	
+	
+	public boolean contains(String key)
+	{
+		return this.data.containsKey(key);
+	}
+	
+	
+	public Object getItem(String key, boolean hasDefault, Object defalt)
+	{
+		if(hasDefault)
+			return this.data.getOrDefault(key, defalt);
+		else
+			return this.data.get(key);
+	}
+	
+	
+	public void setItem(String key, Object value)
+	{
+		this.data.put(key, value);
+	}
+	
+	
+	public void removeItem(String key)
+	{
+		this.data.remove(key);
+	}
+	
+	
+	public Object getItem(String key)
+	{
+		return this.getItem(key, false, null);
+	}
+	
+	
+	public Iterator<String> iterator() {
+		return data.keySet().iterator();
+	}
+	
+	
+	//Equals and HashCode function overrides
+	
+	@Override
+	public boolean equals(Object otherObject)
+	{
+		if(otherObject == null)
+			return false;
+		
+		if(!otherObject.getClass().equals(this.getClass()))
+			return false;
+		
+		
+		StorageToken other = (StorageToken) otherObject;
+		
+		return this.data.equals(other.data);
+	}
+	
+	
+	@Override
+	public int hashCode()
+	{
+		return this.data.hashCode();
+	}
+	
+	
+	//Validation
+	public boolean isValidKey(Object key)
+	{
+		return TokenRWFormat.VALID_KEY_TYPES.contains(key.getClass());
+	}
+	
+	
+	public boolean isValidValue(Object value)
+	{
+		return TokenRWFormat.VALID_VALUE_TYPES.contains(value.getClass());
+	}
+	
+	
+	public boolean isValid()
+	{
+		Object id = data.get(ID_KEY);
+		
+		if(id == null)
+			return false;
+		if(!id.getClass().equals(int.class) && !id.getClass().equals(String.class))
+			return false;
+		
+		Object classId = data.get(CLASS_ID_KEY);
+		
+		if(classId != null)
+		{
+			if(!classId.getClass().equals(String.class))
+				return false;
+		}
+		
+		return true;
+		
+	}
+	
+}
---
+++ SuperGLU/Util/tokenformat/TokenRWFormat.java
@@ -0,0 +1,73 @@
+/**
+ * This class is the framework for specifying how tokens are turned into a specific format (ie. JSON or XML)
+ * The actual code is handled in subclasses.
+ * 
+ * @author auerbach
+ */
+package Util.tokenformat;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import Util.StorageToken;
+
+public abstract class TokenRWFormat {
+
+	public static ArrayList<Class<?>> VALID_KEY_TYPES = new ArrayList<>();
+	public static ArrayList<Class<?>> VALID_VALUE_TYPES = new ArrayList<>();
+	
+	protected static ArrayList<Class<?>> VALID_ATOMIC_VALUE_TYPES = new ArrayList<Class<?>>();
+	protected static ArrayList<Class<?>> VALID_SEQUENCE_TYPES = new ArrayList<>();
+	protected static ArrayList<Class<?>> VALID_MAPPING_TYPES = new ArrayList<>();
+	
+	
+	static
+	{
+		VALID_KEY_TYPES.add(String.class);
+		populateValidAtomicValueTypes();
+		populateValidSequenceTypes();
+		populateValidMappingTypes();
+		VALID_VALUE_TYPES.addAll(VALID_ATOMIC_VALUE_TYPES);
+		VALID_VALUE_TYPES.addAll(VALID_SEQUENCE_TYPES);
+		VALID_VALUE_TYPES.addAll(VALID_MAPPING_TYPES);
+	}
+	
+	
+	private static void populateValidAtomicValueTypes()
+	{
+		VALID_ATOMIC_VALUE_TYPES.add(boolean.class);
+		VALID_ATOMIC_VALUE_TYPES.add(int.class);
+		VALID_ATOMIC_VALUE_TYPES.add(float.class);
+		VALID_ATOMIC_VALUE_TYPES.add(String.class);
+		//VALID_ATOMIC_VALUE_TYPES.add(null);//Not sure if this should be here.  I don't think so.
+	}
+	
+	private static void populateValidSequenceTypes()
+	{
+		VALID_SEQUENCE_TYPES.add(List.class);
+		VALID_SEQUENCE_TYPES.add(ArrayList.class);
+	}
+	
+	
+	private static void populateValidMappingTypes()
+	{
+		VALID_MAPPING_TYPES.add(Map.class);
+		VALID_MAPPING_TYPES.add(HashMap.class);
+	}
+	
+	
+	public static StorageToken parse(String input)
+	{
+		throw new RuntimeException("Method Not Implemented");
+	}
+	
+	
+	public static StorageToken serialize(Object data)
+	{
+		throw new RuntimeException("Method Not Implemented");
+	}
+	
+	
+};
