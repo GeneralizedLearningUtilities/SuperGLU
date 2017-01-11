f7faa6d45f35c67fec868ac0a744d2add5bdc972
tirthmehta <tirthmehta73@gmail.com>
2017-01-11 12:49:00 -0800

    Test commit

---
+++ SuperGLU/Ontology/Field_Data.java
@@ -0,0 +1,17 @@
+package Ontology;
+
+public class Field_Data {
+	
+	private String field_data;
+	
+	public String getFieldData()
+	{
+		return field_data;
+	}
+	
+	public void setFieldData(String data)
+	{
+		field_data=data;
+	}
+
+}
