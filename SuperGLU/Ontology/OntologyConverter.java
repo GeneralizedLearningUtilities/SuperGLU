ab6bfd0eef59fc8938c25740fad36146ef854544
dan100110 <daniel.auerbach@gmail.com>
2017-02-07 16:35:03 -0800

    adjuste the comments in the ontology converter so that they're recognized by JavaDoc generators.

--- SuperGLU/Ontology/OntologyConverter.java
+++ SuperGLU/Ontology/OntologyConverter.java
@@ -1,7 +1,5 @@
 package Ontology;
 
-/*
-
 /**
  * OntologyConverter  Class 
  * This the class that provides the validity check of whether a message is valid or not and then further converts it if its 
@@ -20,117 +18,124 @@ import Util.SerializationConvenience;
 import Util.StorageToken;
 import java.util.*;
 
-public class OntologyConverter {
-	static MessageMap correctMap=null;
-	
-	private List<MessageMap> messageMaps;
-	
-	public OntologyConverter()
-	{
-		messageMaps=null;
-	}
-	// Test code makes a mapping for scenarioName; Mapping is variable testMap; x = [testMap,]
-	public OntologyConverter(List<MessageMap> x)
-	{
-		messageMaps=x;
-	}
-	
-	
-	
-//THE FUNCTION NECESSARY FOR CHECKING WHETHER THE MESSAGE INOUT TOKEN COMING IN HAS A VALID MATCH WITH THE AVAIALABLE SET 
-//	OF MAPPINGS
-	
-	public boolean isValidSourceMsg(BaseMessage b,StorageToken input,String firstwordkey)
-	{
-		//CURRENTLY SINCE THERE IS ONLY 1 MESSAGEMAP IN THE LIST OF MESSAGEMAPS
-				for(MessageMap x:messageMaps)
-				{
-					int count=0;
-					MessageType in=x.getInMsgType();
-					
-					StorageToken ST_inMsgType=in.saveToToken();
-					
-				
-					
-					if(input.getClassId().equals(ST_inMsgType.getItem(in.MESSAGE_TYPE_CLASS_ID_KEY)))
-					{
-						count=1;
-					}
-					MessageTemplate mTemp= in.getMessageTemplate();
-					ArrayList<FieldData> arr=mTemp.getDefaultFieldData();
-					for(FieldData y:arr)
-					{
-						if(y.getFieldData().equals(firstwordkey))
-							{
-								count+=1;
-								correctMap=x;
-								break;
-							}			
-					}
-					if(count==2)
-						return true;
-					
-				}
-				return false;
-	}
-	
-	
-	//THE CONVERT METHOD IS ACTUALLY USED TO PERFORM THE CONVERSION TO THE TARGET MESSAGE OBJECT, ONCE THE VALID MAPPING
-	// HAS BEEN IDENTIFIED
+public class OntologyConverter
+{
+    static MessageMap correctMap = null;
 
-	public BaseMessage convert(BaseMessage b,StorageToken input)
+    private List<MessageMap> messageMaps;
+
+    public OntologyConverter()
+    {
+	messageMaps = null;
+    }
+
+    /**
+     * Test code makes a mapping for scenarioName; Mapping is variable testMap;
+     * x = [testMap,]
+     * 
+     * @param x
+     */
+    public OntologyConverter(List<MessageMap> x)
+    {
+	messageMaps = x;
+    }
+
+    /**
+     * THE FUNCTION NECESSARY FOR CHECKING WHETHER THE MESSAGE INOUT TOKEN
+     * COMING IN HAS A VALID MATCH WITH THE AVAIALABLE SET OF MAPPINGS
+     */
+    public boolean isValidSourceMsg(BaseMessage b, StorageToken input, String firstwordkey)
+    {
+	// CURRENTLY SINCE THERE IS ONLY 1 MESSAGEMAP IN THE LIST OF MESSAGEMAPS
+	for (MessageMap x : messageMaps)
 	{
-		if(correctMap==null)
-			return null;
-		MessageType out=correctMap.getOutMsgType();
-		MessageTemplate mtemp=out.getMessageTemplate();
-		StorageToken target=mtemp.createTargetStorageToken(out.getClassId());
-		
-				
-		ArrayList<FieldMap> mappingList=correctMap.getFieldMappings();
-		for(FieldMap maps: mappingList)
+	    int count = 0;
+	    MessageType in = x.getInMsgType();
+
+	    StorageToken ST_inMsgType = in.saveToToken();
+
+	    if (input.getClassId().equals(ST_inMsgType.getItem(in.MESSAGE_TYPE_CLASS_ID_KEY)))
+	    {
+		count = 1;
+	    }
+	    MessageTemplate mTemp = in.getMessageTemplate();
+	    ArrayList<FieldData> arr = mTemp.getDefaultFieldData();
+	    for (FieldData y : arr)
+	    {
+		if (y.getFieldData().equals(firstwordkey))
 		{
-			
-			NestedAtomic inFields=maps.getInFields();
-			String[] inFieldsIndex=inFields.getIndex();
-			
-			NestedAtomic outFields=maps.getOutFields();
-			String[] outFieldsIndex=outFields.getIndex();
-			
-			String valueToBeInserted="";
-			HashMap<String, String> hmap=new HashMap<>();
-			for(String value:inFieldsIndex)
-			{
-				if(input.contains(value))
-				{
-					
-					valueToBeInserted=(String) input.getItem(value); 
-					
-					hmap.put(value, valueToBeInserted);
-				}
-							
-			}
-			
-			
-			for(String value:outFieldsIndex)
-			{
-				for(String key: hmap.keySet())
-				{		   
-					target.setItem(value,hmap.get(key));
-					System.out.println("check value "+target.getItem(value));
-				}
-			    		    
-			}
-								
+		    count += 1;
+		    correctMap = x;
+		    break;
 		}
-		
-		BaseMessage targetObj=(BaseMessage) SerializationConvenience.untokenizeObject(target);
-		
-		if(targetObj!=null)
-			return targetObj;
-		else
-			return null;
-	
+	    }
+	    if (count == 2)
+		return true;
+
 	}
+	return false;
+    }
+
+    /**
+     * 
+     * THE CONVERT METHOD IS ACTUALLY USED TO PERFORM THE CONVERSION TO THE
+     * TARGET MESSAGE OBJECT, ONCE THE VALID MAPPING HAS BEEN IDENTIFIED
+     * 
+     * @param b
+     * @param input
+     * @return
+     */
+    public BaseMessage convert(BaseMessage b, StorageToken input)
+    {
+	if (correctMap == null)
+	    return null;
+	MessageType out = correctMap.getOutMsgType();
+	MessageTemplate mtemp = out.getMessageTemplate();
+	StorageToken target = mtemp.createTargetStorageToken(out.getClassId());
+
+	ArrayList<FieldMap> mappingList = correctMap.getFieldMappings();
+	for (FieldMap maps : mappingList)
+	{
+
+	    NestedAtomic inFields = maps.getInFields();
+	    String[] inFieldsIndex = inFields.getIndex();
+
+	    NestedAtomic outFields = maps.getOutFields();
+	    String[] outFieldsIndex = outFields.getIndex();
+
+	    String valueToBeInserted = "";
+	    HashMap<String, String> hmap = new HashMap<>();
+	    for (String value : inFieldsIndex)
+	    {
+		if (input.contains(value))
+		{
+
+		    valueToBeInserted = (String) input.getItem(value);
+
+		    hmap.put(value, valueToBeInserted);
+		}
+
+	    }
+
+	    for (String value : outFieldsIndex)
+	    {
+		for (String key : hmap.keySet())
+		{
+		    target.setItem(value, hmap.get(key));
+		    System.out.println("check value " + target.getItem(value));
+		}
+
+	    }
+
+	}
+
+	BaseMessage targetObj = (BaseMessage) SerializationConvenience.untokenizeObject(target);
+
+	if (targetObj != null)
+	    return targetObj;
+	else
+	    return null;
+
+    }
 
 }
