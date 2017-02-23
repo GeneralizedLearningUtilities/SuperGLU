1c5e130de8a0f952dd6882a815fc6f0178098a98
dan100110 <daniel.auerbach@gmail.com>
2017-02-22 19:54:38 -0800

    implemented get and set data functions for NestedAtomic.

--- SuperGLU/Ontology/Mappings/NestedAtomic.java
+++ SuperGLU/Ontology/Mappings/NestedAtomic.java
@@ -17,14 +17,16 @@ import Util.StorageToken;
  * that would potentially map together on both sides of the communication. The
  * keys help the values to be set in the target message fields.
  * 
+ * @author auerbach
  * @author tirthmehta
+ * 
  */
 
 public class NestedAtomic extends Serializable implements FieldData
 {
 
     public static final String NESTED_ATOMIC_INDICES_KEY = "nestedAtomicIndices";
-    
+
     private static final Logger log = LoggerFactory.getLogger(NestedAtomic.class);
 
     private List<Pair<Class<?>, String>> indices;
@@ -102,13 +104,13 @@ public class NestedAtomic extends Serializable implements FieldData
     public void initializeFromToken(StorageToken token)
     {
 	super.initializeFromToken(token);
-	
-	List<Pair<String, String>> indicesWithClassesAsStrings = (List<Pair<String,String>>) SerializationConvenience.untokenizeObject(token.getItem(NESTED_ATOMIC_INDICES_KEY));
+
+	List<Pair<String, String>> indicesWithClassesAsStrings = (List<Pair<String, String>>) SerializationConvenience.untokenizeObject(token.getItem(NESTED_ATOMIC_INDICES_KEY));
 	this.indices = new ArrayList<>();
-	
+
 	for (Pair<String, String> indexWithClassAsString : indicesWithClassesAsStrings)
 	{
-	    if(indexWithClassAsString.getFirst() != null)
+	    if (indexWithClassAsString.getFirst() != null)
 	    {
 		try
 		{
@@ -117,13 +119,13 @@ public class NestedAtomic extends Serializable implements FieldData
 		    this.indices.add(index);
 		} catch (ClassNotFoundException e)
 		{
-		    //If we can't parse the class then skip this index.
+		    // If we can't parse the class then skip this index.
 		    e.printStackTrace();
 		    continue;
-		}	
+		}
 	    }
 	}
-	
+
     }
 
     @Override
@@ -143,52 +145,152 @@ public class NestedAtomic extends Serializable implements FieldData
 	result.setItem(NESTED_ATOMIC_INDICES_KEY, SerializationConvenience.tokenizeObject(classesAsStrings));
 	return result;
     }
-    
 
     @Override
     public Object retrieveFieldData(StorageToken msg)
     {
 	Object currentContainer = msg;
-	
-	for(Pair<Class<?>, String> intermediateField : this.indices)
+
+	for (Pair<Class<?>, String> intermediateField : this.indices)
 	{
-	    //Object does not exist, return null
-	    if(currentContainer == null)
+	    // Object does not exist, return null
+	    if (currentContainer == null)
 		return null;
-	    
-	    if(currentContainer instanceof StorageToken)
+
+	    if (currentContainer instanceof StorageToken)
 	    {
 		StorageToken containerAsStorageToken = (StorageToken) currentContainer;
 		currentContainer = containerAsStorageToken.getItem(intermediateField.getSecond());
-	    }
-	    else if(currentContainer instanceof List<?>)
+	    } else if (currentContainer instanceof List<?>)
 	    {
 		List<?> containerAsList = (List<?>) currentContainer;
 		int listIndex = Integer.parseInt(intermediateField.getSecond());
 		currentContainer = containerAsList.get(listIndex);
-	    }
-	    else if(currentContainer instanceof Map<?, ?>)
-	    {//This code will currently assume that the Map has a key of Strings for the moment. 
+	    } else if (currentContainer instanceof Map<?, ?>)
+	    {// This code will currently assume that the Map has a key of
+	     // Strings for the moment.
 		Map<String, Object> containerAsMap = (Map<String, Object>) currentContainer;
 		currentContainer = containerAsMap.get(intermediateField.getSecond());
-	    }
-	    else
+	    } else
 	    {
-		//TODO: return null or throw exception?
+		// TODO: return null or throw exception?
 		String warning = "index:" + intermediateField.getSecond() + " does not exist in StorageContainer: " + currentContainer.toString();
 		NestedAtomic.log.warn(warning);
 		throw new RuntimeException(warning);
 	    }
 	}
-	
+
 	Object result = SerializationConvenience.untokenizeObject(currentContainer);
 	return result;
+    }
+
+    private Object createNextContainer(Class<?> clazz)
+    {
+	try
+	{
+	    Object nextContainerUntokenized = clazz.newInstance();
+	    Object result = SerializationConvenience.tokenizeObject(nextContainerUntokenized);
+	    return result;
+	} catch (InstantiationException | IllegalAccessException e)
+	{
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	    log.warn("failed to create object of type: " + clazz.getName());
+	    throw new RuntimeException(e);
+	}
+
     }
 
     @Override
     public void storeData(StorageToken msg, Object data)
     {
-	
+	Object currentContainer = msg;
+
+	// if the message is null just stop
+	if (msg == null)
+	{
+	    log.warn("attempted to store data to a null object, failing gracefully");
+	    return;
+	}
+	// Tokenize the object (probably not necessary, but good to make sure)
+
+	Object tokenizedData = SerializationConvenience.tokenizeObject(data);
+
+	// We only want to drill down to the second to last item in the list.
+	for (Pair<Class<?>, String> intermediateContainerData : this.indices.subList(0, this.indices.size() - 2))
+	{
+	    Object nextContainer;
+	    // Object does not exist. We'll have to create it.
+	    if (currentContainer instanceof StorageToken)
+	    {
+		StorageToken containerAsStorageToken = (StorageToken) currentContainer;
+		nextContainer = containerAsStorageToken.getItem(intermediateContainerData.getSecond());
+
+		if (nextContainer == null)
+		{
+		    Class<?> clazz = intermediateContainerData.getFirst();
+		    nextContainer = createNextContainer(clazz);
+		    containerAsStorageToken.setItem(intermediateContainerData.getSecond(), nextContainer);
+		}
+
+	    } else if (currentContainer instanceof List<?>)
+	    {
+		List<Object> containerAsList = (List<Object>) currentContainer;
+		int listIndex = Integer.parseInt(intermediateContainerData.getSecond());
+		nextContainer = containerAsList.get(listIndex);
+
+		if (nextContainer == null)
+		{
+		    Class<?> clazz = intermediateContainerData.getFirst();
+		    nextContainer = createNextContainer(clazz);
+		    containerAsList.add(listIndex, nextContainer);
+		}
+
+	    } else if (currentContainer instanceof Map<?, ?>)
+	    {// This code will currently assume that the Map has a key of
+	     // Strings for the moment.
+		Map<String, Object> containerAsMap = (Map<String, Object>) currentContainer;
+		nextContainer = containerAsMap.get(intermediateContainerData.getSecond());
+
+		if (nextContainer == null)
+		{
+		    Class<?> clazz = intermediateContainerData.getFirst();
+		    nextContainer = createNextContainer(clazz);
+		    containerAsMap.put(intermediateContainerData.getSecond(), nextContainer);
+		}
+
+	    } else
+	    {// We've got a primitive. We'll have to
+	     // TODO: Adjust this so that it handles things properly. Can't
+	     // think right now.
+		String warning = "index:" + intermediateContainerData.getSecond() + " already exists as a different type in StorageContainer: " + currentContainer.toString();
+		NestedAtomic.log.warn(warning);
+
+		nextContainer = this.createNextContainer(intermediateContainerData.getFirst());
+
+		if (currentContainer instanceof StorageToken)
+		    ((StorageToken) currentContainer).setItem(intermediateContainerData.getSecond(), nextContainer);
+		else if (currentContainer instanceof List<?>)
+		    ((List) currentContainer).add(Integer.parseInt(intermediateContainerData.getSecond()), nextContainer);
+		else if (currentContainer instanceof Map<?, ?>)
+		    ((Map) currentContainer).put(intermediateContainerData.getSecond(), nextContainer);
+
+	    }
+
+	    currentContainer = nextContainer;
+	}
+
+	String fieldName = this.indices.get(this.indices.size() - 1).getSecond();
+
+	if (fieldName != null)
+	{
+	    if (currentContainer instanceof StorageToken)
+		((StorageToken) currentContainer).setItem(fieldName, tokenizedData);
+	    else if (currentContainer instanceof List<?>)
+		((List) currentContainer).add(Integer.parseInt(fieldName), tokenizedData);
+	    else if (currentContainer instanceof Map<?, ?>)
+		((Map) currentContainer).put(fieldName, tokenizedData);
+	}
     }
 
 }
