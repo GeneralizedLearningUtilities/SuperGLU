14e968696c24f08bf8eb85514238b947c6d15b20
dan100110 <daniel.auerbach@gmail.com>
2017-09-06 14:50:01 -0700

    added code to launch virtual human demo through gift.

--- SuperGLU/Examples/VHuman/GIFTVHumanBridge.java
+++ SuperGLU/Examples/VHuman/GIFTVHumanBridge.java
@@ -1,47 +1,46 @@
 package Examples.VHuman;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import Core.BaseMessage;
+import Core.GIFTMessage;
 import Core.MessagingGateway;
 import Core.VHMessage;
 import Core.Config.ServiceConfiguration;
+import edu.usc.ict.vhmsg.MessageEvent;
+import edu.usc.ict.vhmsg.MessageListener;
 import edu.usc.ict.vhmsg.VHMsg;
 
 /**
  * This class is the internal service that will route received virtual human
- * messages to vhmsg.  It will also attempt to convert non-VHuman messages  into VHuman messages.
+ * messages to vhmsg. It will also attempt to convert non-VHuman messages into
+ * VHuman messages.
  * 
  * @author auerbach
  *
  */
 
 public class GIFTVHumanBridge extends MessagingGateway {
-	
+
 	public static final String BROKER_HOST_PARAM_KEY = "brokerHost";
 	public static final String BROKER_PORT_PARAM_KEY = "brokerPort";
-	
-	
+
 	protected VHMsg vhmsg;
-	
-	public GIFTVHumanBridge(ServiceConfiguration config)
-	{
+
+	public GIFTVHumanBridge(ServiceConfiguration config) {
 		super(config.getId(), null, null, null, null);
-		
-		
-		String brokerHost = (String)config.getParams().get(BROKER_HOST_PARAM_KEY);
-		int brokerPort = (int)config.getParams().get(BROKER_PORT_PARAM_KEY);
-		
-		
+
+		String brokerHost = (String) config.getParams().get(BROKER_HOST_PARAM_KEY);
+		int brokerPort = (int) config.getParams().get(BROKER_PORT_PARAM_KEY);
+
 		this.vhmsg = new VHMsg(brokerHost, Integer.toString(brokerPort), "DEFAULT_SCOPE");
 		this.vhmsg.openConnection();
 
-
-		
 	}
-	
-	
+
 	public GIFTVHumanBridge(String brokerURL) {
 		super("GIFT_VHUMAN_BRIDGE", null, null, null, null);
-
 
 		String brokerHost = brokerURL.split(":")[1].split("//")[1];
 		String brokerPort = brokerURL.split(":")[2];
@@ -54,14 +53,41 @@ public class GIFTVHumanBridge extends MessagingGateway {
 	public void receiveMessage(BaseMessage msg) {
 		super.receiveMessage(msg);
 
-		// attempt to convert the recieved message to a vhuman message.
-		BaseMessage convertedMessage = super.convertMessages(msg, VHMessage.class);
+		if (msg instanceof GIFTMessage) {
 
-		if (convertedMessage != null && convertedMessage instanceof VHMessage) {
-			VHMessage convertedVHMessage = (VHMessage) convertedMessage;
+			// attempt to convert the recieved message to a vhuman message.
+			BaseMessage convertedMessage = super.convertMessages(msg, VHMessage.class);
 
-			// send out the message if it was successfully converted.
-			vhmsg.sendMessage(convertedVHMessage.getFirstWord(), convertedVHMessage.getBody());
+			if (convertedMessage != null && convertedMessage instanceof VHMessage) {
+				VHMessage convertedVHMessage = (VHMessage) convertedMessage;
+
+				// send out the message if it was successfully converted.
+				vhmsg.sendMessage(convertedVHMessage.getFirstWord(), convertedVHMessage.getBody());
+			}
+		} else if (msg instanceof VHMessage) {
+			BaseMessage convertedMessage = super.convertMessages(msg, GIFTMessage.class);
+
+			if (convertedMessage != null && convertedMessage instanceof GIFTMessage) {
+				this.sendMessage(convertedMessage);
+			}
+
+			startVHuman((VHMessage) msg);
+		}
+	}
+
+	// Special code to handle the startup of the virtual human
+	private void startVHuman(VHMessage vhMessage) {
+		Runtime rt = Runtime.getRuntime();
+
+		try {
+			if (vhMessage.getFirstWord().equals("vrComponent") && vhMessage.getBody().equals("nvb parser")) {
+				rt.exec("../VHuman/gift/02a-launch-unity-house.bat");
+			}
+			if (vhMessage.getFirstWord().equals("vrSpeech") && vhMessage.getBody().equals("interp user0001 1 1.0 normal THEREMIN NOSFERATU THERMOCOUPLE PATRONUS"))
+				rt.exec("../VHuman/gift/03b-spawn-brad.bat");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
