3631a56ed57361837345457a5382aba6ca543ab0
Nitin Surana <nsurana@ict.usc.edu>
2017-10-24 16:27:38 -0700

    Renamed HttpMessagingGateway to SocketIOGateway

--- java_maven/src/main/java/edu/usc/ict/superglu/core/config/Scratch.java
+++ java_maven/src/main/java/edu/usc/ict/superglu/core/config/Scratch.java
@@ -1,7 +1,7 @@
 package edu.usc.ict.superglu.core.config;
 
 import edu.usc.ict.superglu.core.ActiveMQTopicConfiguration;
-import edu.usc.ict.superglu.core.HTTPMessagingGateway;
+import edu.usc.ict.superglu.core.SocketIOGateway;
 import edu.usc.ict.superglu.core.MessagingGateway;
 import edu.usc.ict.superglu.util.SerializationConvenience;
 import edu.usc.ict.superglu.util.SerializationFormatEnum;
@@ -35,12 +35,12 @@ public class Scratch {
 		amqParams.put(MessagingGateway.GATEWAY_BLACKLIST_KEY, amqGatewayBlackList);
 		
 		String amqGatewayName = "activeMQGateway";
-		String socketioGatewayName = "socketIOGateway";
+		String socketIOGatewayName = "socketIOGateway";
 		String bridgeGatewayName = "VHMSGBridgeGateway";
 		
 		List<String> amqNodes = new ArrayList<>();
 		
-		amqNodes.add(socketioGatewayName);
+		amqNodes.add(socketIOGatewayName);
 		amqNodes.add(bridgeGatewayName);
 		
 		ServiceConfiguration activeMQGateway = new ServiceConfiguration(amqGatewayName, ActiveMQTopicConfiguration.class, amqParams, amqNodes, new ArrayList<>(), new ArrayList<>());
@@ -53,7 +53,7 @@ public class Scratch {
 		socketIONodes.add(amqGatewayName);
 		socketIONodes.add(bridgeGatewayName);
 		
-		ServiceConfiguration socketIOGateway = new ServiceConfiguration(socketioGatewayName, HTTPMessagingGateway.class, socketIOParams, socketIONodes, new ArrayList<>(), new ArrayList<>());
+		ServiceConfiguration socketIOGateway = new ServiceConfiguration(socketIOGatewayName, SocketIOGateway.class, socketIOParams, socketIONodes, new ArrayList<>(), new ArrayList<>());
 		
 		
 		Map<String, Object> defaultBridgeParams = new HashMap<>();
@@ -62,7 +62,7 @@ public class Scratch {
 		
 		List<String> bridgeNodes = new ArrayList<>();
 		bridgeNodes.add(amqGatewayName);
-		bridgeNodes.add(socketioGatewayName);
+		bridgeNodes.add(socketIOGatewayName);
 		
 		ServiceConfiguration GiftVHumanBridge = new ServiceConfiguration("defaultBridge", GIFTVHumanBridge.class, defaultBridgeParams, bridgeNodes, new ArrayList<>(), new ArrayList<>());
 		
