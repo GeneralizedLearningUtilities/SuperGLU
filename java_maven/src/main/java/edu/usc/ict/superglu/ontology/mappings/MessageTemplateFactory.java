e0b4e29b4489fe514757e4ef9036651fbd4c6001
dan100110 <daniel.auerbach@gmail.com>
2017-12-07 16:10:24 -0800

    First cut of the MessageTemplate factory.

---
+++ java_maven/src/main/java/edu/usc/ict/superglu/ontology/mappings/MessageTemplateFactory.java
@@ -0,0 +1,56 @@
+package edu.usc.ict.superglu.ontology.mappings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.usc.ict.superglu.util.Pair;
+import edu.usc.ict.superglu.util.StorageToken;
+
+/**
+ * This class is responsible for converting a storage token into a message template.
+ * 
+ * It will recursively go through all the fields and construct a 
+ * @author auerbach
+ *
+ */
+
+public class MessageTemplateFactory {
+
+	
+	public static MessageTemplate createMessageTemplate(StorageToken token, List<String> topLevelExcludedFields, List<String> globalExcludedFields)
+	{
+		List<Pair<Class<?>, String>> path = new ArrayList<>();
+		List<Pair<FieldData, Object>> templateData = new ArrayList<>();
+
+		populateFields(path, token, templateData, topLevelExcludedFields, globalExcludedFields);
+
+		MessageTemplate template = new MessageTemplate(templateData);
+
+		return template;
+	}
+	
+	
+	private static void populateFields(List<Pair<Class<?>,String>> path, StorageToken currentStorageToken, List<Pair<FieldData, Object>> templateData, List<String> topLevelExcludedFields, List<String> globalExcludedFields)
+	{
+		for (String currentField : currentStorageToken) {
+			Object value = currentStorageToken.getItem(currentField);
+
+			if (value instanceof StorageToken) {
+				path.add(new Pair<Class<?>, String>(StorageToken.class, currentField));
+				populateFields(path, (StorageToken) value, templateData, new ArrayList<>(), globalExcludedFields);
+				path.remove(path.size() - 1);
+			} else {
+				if (globalExcludedFields.contains(currentField) || topLevelExcludedFields.contains(currentField))
+					continue;// skip to the next field
+
+				List<Pair<Class<?>, String>> copyOfPath = new ArrayList<>();
+				copyOfPath.addAll(path);//use a copy of the path instead of the one that the function will be modifiying.
+				copyOfPath.add(new Pair<Class<?>, String>(value.getClass(), currentField));
+				NestedAtomic field = new NestedAtomic(copyOfPath);
+
+				templateData.add(new Pair<FieldData, Object>(field, value));
+			}
+		}
+	}
+	
+}
