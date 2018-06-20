7d9138c8a0ee0582e423f4c36a286f25bfb61a1f
Alicia Tsai <32806743+alicia-tsai@users.noreply.github.com>
2018-06-19 17:30:35 -0700

    - Fix bad request issue when connecting to learn locker

--- python_module/SuperGLU/Services/LoggingService/LearnLockerConnection.py
+++ python_module/SuperGLU/Services/LoggingService/LearnLockerConnection.py
@@ -7,6 +7,7 @@ from SuperGLU.Core.MessagingGateway import BaseService
 from SuperGLU.Services.LoggingService.Constants import XAPI_LOG_VERB
 import requests
 import uuid
+import json
 
 
 class LearnLockerConnection(BaseService):
@@ -15,21 +16,29 @@ class LearnLockerConnection(BaseService):
         super(LearnLockerConnection, self).__init__(gateway=gateway)
         self._url = url
         self._key = key
-
         self.logFile = open("log.txt", 'w')
-
+        self.errorLog = open("errorLog.txt", "w")
 
     def receiveMessage(self, msg):
         super(LearnLockerConnection, self).receiveMessage(msg)
 
         if msg.getVerb() == XAPI_LOG_VERB:
             statementAsJson = msg.getResult()
-            headerDict = {'Authorization' : self._key,\
-                       'X-Experience-API-Version': '1.0.3',\
-                       'Content-Type' : 'application/json',\
-                      }
-            response = requests.put(url=self._url + '/data/xAPI/statements?statementId=' + str(uuid.uuid4()), data=statementAsJson, headers=headerDict )
-            print(str(response))
+            headerDict = {'Authorization' : self._key,
+                          'X-Experience-API-Version': '1.0.3',
+                          'Content-Type' : 'application/json'
+                          }
+            statement = json.loads(statementAsJson)
+            statement['context']['extensions'] = {}
+            statement['object']['id'] = "http://example.com/activities/solo-hang-gliding"
+            statement['actor'].pop('openid', None)
+            response = requests.put(url=self._url + '/data/xAPI/statements?statementId=' + str(uuid.uuid4()), data=json.dumps(statement), headers=headerDict)
+            if str(response) == "<Response [400]>":
+                print(str(response), response.text)
+                self.errorLog.write(response.text)
+                self.errorLog.write("\n")
+            #response.raise_for_status()
 
+            # write to log file
             self.logFile.write(statementAsJson)
             self.logFile.write("\n")
