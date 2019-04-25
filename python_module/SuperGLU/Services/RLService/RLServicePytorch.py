a71a9105532109f8577519547fb3219b6e42cb84
Raj <thakeraj01@gmail.com>
2019-04-25 15:11:54 -0700

    Removing unwanted comments

--- python_module/SuperGLU/Services/RLService/RLServicePytorch.py
+++ python_module/SuperGLU/Services/RLService/RLServicePytorch.py
@@ -45,70 +45,7 @@ class NeuralNetwork(nn.Module):
         out = F.softmax(out, dim=1)
         return out
 
-    #
-    # def __init__(self):
-    #     super(NeuralNetwork, self).__init__()
-    #     self.number_of_actions = 3
-    #     self.gamma = 0.99
-    #     self.final_epsilon = 0.0001
-    #     self.initial_epsilon = 0.1
-    #     self.number_of_iterations = 200
-    #     self.replay_memory_size = 10000
-    #     self.minibatch_size = 32
-    #     self.conv1 = nn.Conv2d(4, 32, 8, 4)
-    #     self.relu1 = nn.ReLU(inplace=True)
-    #     self.conv2 = nn.Conv2d(32, 64, 4, 2)
-    #     self.relu2 = nn.ReLU(inplace=True)
-    #     self.conv3 = nn.Conv2d(64, 64, 3, 1)
-    #     self.relu3 = nn.ReLU(inplace=True)
-    #     self.fc4 = nn.Linear(3136, 512)
-    #     self.relu4 = nn.ReLU(inplace=True)
-    #     self.fc5 = nn.Linear(512, self.number_of_actions)
-    #     self.affine1 = nn.Linear(4, 128)
-    #     self.dropout = nn.Dropout(p=0.6)
-    #     self.affine2 = nn.Linear(128, 2)
-    #
-    #     self.saved_log_probs = []
-    #     self.rewards = []
-    #
-    # def forward(self, x):
-    #     x = self.affine1(x)
-    #     x = self.dropout(x)
-    #     x = F.relu(x)
-    #     action_scores = self.affine2(x)
-    #     return F.softmax(action_scores, dim=1)
-
-    #     super(NeuralNetwork, self).__init__()
-    #     self.number_of_actions = 3
-    #     self.gamma = 0.99
-    #     self.final_epsilon = 0.0001
-    #     self.initial_epsilon = 0.1
-    #     self.number_of_iterations = 200
-    #     self.replay_memory_size = 10000
-    #     self.minibatch_size = 32
-    #     self.conv1 = nn.Conv2d(1, 32, 8, 1)
-    #     self.relu1 = nn.ReLU(inplace=True)
-    #     self.conv2 = nn.Conv2d(32, 64, 4, 2)
-    #     self.relu2 = nn.ReLU(inplace=True)
-    #     self.conv3 = nn.Conv2d(64, 64, 3, 1)
-    #     self.relu3 = nn.ReLU(inplace=True)
-    #     self.fc4 = nn.Linear(3136, 512)
-    #     self.relu4 = nn.ReLU(inplace=True)
-    #     self.fc5 = nn.Linear(512, self.number_of_actions)
-    #
-    # def forward(self, x):
-    #     out = self.conv1(x)
-    #     out = self.relu1(out)
-    #     out = self.conv2(out)
-    #     out = self.relu2(out)
-    #     out = self.conv3(out)
-    #     out = self.relu3(out)
-    #     out = out.view(out.size()[0], -1)
-    #     out = self.fc4(out)
-    #     out = self.relu4(out)
-    #     out = self.fc5(out)
-    #     return out
-
+    
 class State:
     def __init__(self):
         self.score = self.playerIndex = self.loopIter = 0
